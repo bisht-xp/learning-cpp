f38372611821f24000c207e55e8eb10454bd4c2b
bisht-xp <bishtkamal2907@gmail.com>
2024-01-20 21:43:28 +0530

    Add: add reverse and delete linked list

--- linkedList/firstLinkedList.cpp
+++ linkedList/firstLinkedList.cpp
@@ -35,7 +35,7 @@ void insertAtEnd(Node *&head, int value) {
     temp->next = node;
 }
 
-void insertAtMiddle(Node *head, int value, int pos) {
+void insertAtMiddle(Node *&head, int value, int pos) {
     Node *n = new Node(value);
     Node *temp = head;
     for(int i=0; i<pos-1; i++) {
@@ -44,6 +44,48 @@ void insertAtMiddle(Node *head, int value, int pos) {
     Node *node = temp->next;
     temp->next = n;
     n->next = node;
+}
+
+void deleteNode(Node *&head, int pos) {
+    if(head == NULL) {
+        return;
+    }
+    Node *temp = head;
+    for(int i=0; i<pos-2; i++) {
+        temp = temp->next;
+    }
+    Node *n = temp->next;
+    temp->next = n->next;
+
+    delete n;
+}
+
+void reverse(Node *&head) {
+    if(head == NULL) {
+        return;
+    }
+    Node *prev = NULL;
+    Node* curr = head;
+    Node *temp;
+
+    while(curr != NULL) {
+        temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    head = prev;
+}
+
+Node* reverseRecussive(Node* &head) {
+    if( head == NULL || head->next == NULL ) {
+        return head;
+    } 
+
+    Node *newNode = reverseRecussive(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return newNode;
 }
 
 void display(Node *head) {
@@ -64,5 +106,9 @@ int main() {
     insertAtMiddle(head, 32, 1);
     insertAtBegining(head, 1);
     display(head);
+    // deleteNode(head, 1);
+    Node *newNode =  reverseRecussive(head);
+
+    display(newNode);
     return 0;
 }
