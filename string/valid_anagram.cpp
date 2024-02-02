dd6d32d0330d20bad35fa6696256649dbaf6ab71
bisht-xp <bishtkamal2907@gmail.com>
2024-02-02 21:19:46 +0530

    update: using the better approch

--- string/valid_anagram.cpp
+++ string/valid_anagram.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-bool isAnagram(string s, string t)
+/* bool isAnagram(string s, string t)
 {
 	unordered_map<char, int> map;
 	if(s.length() != t.length()) {
@@ -24,12 +25,31 @@ bool isAnagram(string s, string t)
 	}
 
 	return true;
+}*/
+
+bool isAnagram(string s, string t) {
+	vector<int> v(26,0);
+	if(s.length() != t.length()) {
+		return false;
+	}
+	for(auto c: s) {
+		v[c-'a']++;
+		v[c-'a']--;
+	}
+	for(auto i: v) {
+		if(i != 0) {
+			return false;
+		}
+	}
+	return true;
+
+
 }
 
 int main()
 {
-	string s = "car";
-	string t = "rat";
+	string s = "rat";
+	string t = "car";
 	cout << isAnagram(s, t) << endl;
 	return 0;
 }
