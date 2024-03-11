90f146c9334984f217c8950e5b611aeea19dba0d
bisht-xp <bishtkamal2907@gmail.com>
2024-03-11 22:00:40 +0530

    modified: change the output based on lexiographically or length base 1. helpful in competitive coding

--- backtracking/finding_subset.cpp
+++ backtracking/finding_subset.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -17,28 +19,46 @@ void findingSubset(string input, string output ) {
     return;
 }
 
+
 // modifing existing code try to make it easy to understand
-void findSubset(string input, string output, int i) {
+void findSubset(string input, string output, int i, vector<string> &list) {
     if(i>input.length()-1) {
-        if(output.empty()) {
-            cout << "NULL "<< endl;
-            return;
-        }
-        cout << output << endl;
+        // if(output.empty()) {
+        //     cout << "NULL "<< endl;
+        //     return;
+        // }
+        // cout << output << endl;
+        list.push_back(output);
         return;
     }
 
-    findSubset(input, output+input[i], i+1); 
-    findSubset(input, output, i+1);
+    findSubset(input, output+input[i], i+1, list); 
+    findSubset(input, output, i+1, list);
     return;
 }
 
+bool compare(string a, string b) {
+    if(a.length() == b.length()) {
+        return a < b;
+    }
+    return a.length() < b.length();
+}
 
 int main() {
     string input = "ABC";
+    vector<string> s;
     
     string output = "";
     // findingSubset(input, output);
-    findSubset(input, output,0);
+
+    findSubset(input, output,0, s);
+
+    sort(s.begin(), s.end(), compare);
+
+
+    for(string str: s) {
+        cout << str << ", ";
+    }
+    cout << endl;
     return 0;
 }
