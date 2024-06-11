071cc108d815a3af20a94f86fab39ee14d5f969c
bisht-xp <bishtkamal2907@gmail.com>
2024-06-11 20:06:55 +0530

    added reverse a number using stack

---
+++ stack/reverse_number_stack.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+void insertAtBottom(stack<int> &st, int n) {
+    if(st.empty()) {
+        st.push(n);
+        return;
+    }
+    int temp = st.top();
+    st.pop();
+    insertAtBottom(st, n);
+    st.push(temp);
+}
+
+void reverseStack(stack<int> &st) {
+    if(st.empty()) {
+        return;
+    }
+    int temp = st.top();
+    st.pop();
+    reverseStack(st);
+    insertAtBottom(st, temp);
+ }
+
+
+int reverse(int n){
+	stack<int> st;
+	while(n > 0) {
+	    int lastNumber = n%10;
+	    st.push(lastNumber);
+	    n = n/10;
+	}
+    int rev = 0;
+    int i = 1;
+      
+    while (!st.empty()) 
+    {
+        rev = rev + (st.top() * i);
+        st.pop();
+        i = i * 10;
+    }
+    return rev;
+}
+
+
+int main() {
+    int n = 456;
+    cout << reverse(n) << endl;
+    return 0;
+}
